e2a59d3af7a305a9ab39dc7e2eb33105e4436dda
Cleiver <creibis10@gmail.com>
2023-01-06 12:10:36 -0300

    upload arquivos sem aplicação js

---
+++ script.js
@@ -0,0 +1,8 @@
+function verde(){
+
+    const opcoes = document.querySelector(".opcoes")
+    opcoes.classList.contains(verde)
+
+    const div = document.querySelector(".opcao")
+    div.classList.add("verde")
+}
