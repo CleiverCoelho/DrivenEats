ac47c8500a0ef5e6b614f63ce1e18a094ef303df
Cleiver <creibis10@gmail.com>
2023-01-06 14:54:23 -0300

    funcao de checko opcao adicionada/sem implementacao ion-icon

--- script.js
+++ script.js
@@ -1,8 +1,68 @@
-function verde(){
+function verde(opcao, produto, ioordem){    
+    let tagCheckmark;
+    let bordaCheck;
+    const checkPrato = produto == '.prato';
+    const checkBebida = produto == '.bebida';
+    const checkSobremesa = produto == '.sobremesa';
 
-    const opcoes = document.querySelector(".opcoes")
-    opcoes.classList.contains(verde)
+    // let tagSelecionado;
+    // Procurar se existem ion-icons ligados
+    if(checkPrato){
+        // Procurar se existem bordas check ligadas
+        bordaCheck = document.querySelector(".prato .check")
+        if(bordaCheck != null){
+            bordaCheck.classList.remove("check")
+            bordaCheck.classList.add("borda-transparente")
 
-    const div = document.querySelector(".opcao")
-    div.classList.add("verde")
+        }
+        // tagSelecionado = document.querySelector(".prato ion-icon")
+        else if(ioordem == '.io1'){
+            tagCheckmark = document.querySelector(".prato .io1");
+        }else if(ioordem == '.io2'){
+            tagCheckmark = document.querySelector(".prato .io2");
+        }else if(ioordem == '.io3'){
+            tagCheckmark = document.querySelector(".prato .io3");
+        }
+    }else if(checkBebida){
+        // Procurar se existem bordas check ligadas
+        bordaCheck = document.querySelector(".bebida .check")
+        if(bordaCheck != null){
+            bordaCheck.classList.remove("check")
+            bordaCheck.classList.add("borda-transparente")
+
+        }
+        // tagSelecionado = document.querySelector(".prato ion-icon")
+        else if(ioordem == '.io1'){
+            tagCheckmark = document.querySelector(".bebida .io1");
+        }else if(ioordem == '.io2'){
+            tagCheckmark = document.querySelector(".bebida .io2");
+        }else if(ioordem == '.io3'){
+            tagCheckmark = document.querySelector(".bebida .io3");
+        }
+    }else if(checkSobremesa){
+        // Procurar se existem bordas check ligadas
+        bordaCheck = document.querySelector(".sobremesa .check")
+        if(bordaCheck != null){
+            bordaCheck.classList.remove("check")
+            bordaCheck.classList.add("borda-transparente")
+
+        }
+        // tagSelecionado = document.querySelector(".prato ion-icon")
+        else if(ioordem == '.io1'){
+            tagCheckmark = document.querySelector(".sobremesa .io1");
+        }else if(ioordem == '.io2'){
+            tagCheckmark = document.querySelector(".sobremesa .io2");
+        }else if(ioordem == '.io3'){
+            tagCheckmark = document.querySelector(".sobremesa .io3");
+        }
+    }
+
+    // Para ativar borda verde de check
+    opcao.classList.remove("borda-transparente");
+    opcao.classList.add("check");
+    // Para ativar/desativar ion-icon
+    tagCheckmark.classList.toggle("selecionado");
+    console.log("tag retornada: ");
+    console.log(bordaCheck);
+
 }
